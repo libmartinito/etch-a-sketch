e16e52176b761057bf7fc8edfdb294e1d3fa21a4
Lib Martinito <lib.martinito@gmail.com>
2020-05-24 08:05:56 -0400

    Added a clear button and increased sketch area

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@ const container = document.createElement('div');
 container.className = 'container';
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
-container.style.width = '25vw';
-container.style.height = '25vw';
+container.style.width = '40vw';
+container.style.height = '40vw';
 container.style.margin = '0 auto';
+container.style.border = 'solid';
+container.style.borderColor = '#eeeeee';
 document.body.insertBefore(container,document.querySelector('script'));
 
 // Add a grid based on the number input by the user
@@ -13,9 +15,9 @@ const number = prompt("How many squares per side for the grid?");
 const divWidth = 1 / number * 100;
 const divpiece = document.createElement('div');
 divpiece.className = 'divpiece';
-divpiece.style.backgroundColor = '#393e46';
-divpiece.style.width = divWidth.toString() + '%';
-divpiece.style.height = divWidth.toString() + '%';
+divpiece.style.backgroundColor = '#222831';
+divpiece.style.width = "calc(" + divWidth + "%)";
+divpiece.style.height = "calc(" + divWidth + "%)";
 container.appendChild(divpiece);
 for (let i = 0; i <= number * number - 2; i++) {
   const divClone = divpiece.cloneNode(true);
@@ -24,7 +26,6 @@ for (let i = 0; i <= number * number - 2; i++) {
 
 // Add a hover effect for each div
 const piece = document.querySelectorAll('.container div');
-// piece.addEventListener('mouseover', color, false);
 piece.forEach(piece => piece.addEventListener('mouseover', color, false));
 function color (e) {
   e.target.style.backgroundColor = '#00adb5';
@@ -32,3 +33,25 @@ function color (e) {
 
 // Styling the page
 document.body.style.backgroundColor = '#222831';
+
+// Adding functional buttons
+const buttonContainer = document.createElement('div');
+buttonContainer.className = 'button-container';
+buttonContainer.style.display = 'flex';
+buttonContainer.style.justifyContent = 'center';
+buttonContainer.style.alignItems = 'center';
+buttonContainer.style.height  = '100px';
+const clear = document.createElement('button');
+clear.id = 'clear';
+clear.innerText = 'Clear';
+clear.style.fontWeight = 'bold';
+clear.style.padding = '5px 20px';
+clear.style.border = 'none';
+clear.style.backgroundColor = '#393e46';
+clear.style.color = '#eeeeee';
+buttonContainer.appendChild(clear);
+document.body.insertBefore(buttonContainer,document.querySelector('script'));
+
+document.getElementById('clear').addEventListener('click',function(){
+  piece.forEach(piece => piece.style.backgroundColor = '#222831');
+});
